4ff1a501bd76341a1e877342e2355ee506be0318
Abinaya Sakthivel <40523408+abinayasak@users.noreply.github.com>
2021-02-23 14:21:16 +0100

    Add files via upload

--- fitzhugh_nagumo.py
+++ fitzhugh_nagumo.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
+def dvdt(v, t):
+    A = 0.04  # Constant
+    v_rest = -85.0  # [mV]
+    v_th = -65.0  # [mV]
+    v_peak = 40.0  # [mV]
+    return -A * A * (v - v_rest) * (v - v_th) * (v - v_peak)
+
+
 def fitzhugh_nagumo(v, t):
     a = 0.13; b = 0.013
     c1 = 0.26; c2 = 0.1; c3 = 1.0
@@ -17,11 +25,7 @@ def fitzhugh_nagumo(v, t):
     return dvdt, dwdt
 
 
-
 def set_initial_condition(Nx, V, mesh, tn, theta, dt):
-    v_values = np.zeros(Nx + 1)
-    w_values = np.zeros(Nx + 1)
-
     u0 = []
     element = V.element()
     for cell in cells(mesh):
@@ -37,12 +41,12 @@ def set_initial_condition(Nx, V, mesh, tn, theta, dt):
     u0[u0 < 0.2] = 0.0  # if x < 0.2, set u0 = 0.
     u0[u0 >= 0.2] = -85.0  # if x >= 0.2, set u0 = -85.
 
-    w0 = np.ones(len(u0))
+    w0 = np.zeros(len(u0))
 
     return u0, w0
 
 
-def step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta):
+def step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta, derivative):
     u = TrialFunction(V)
     v = TestFunction(V)
 
@@ -52,111 +56,51 @@ def step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta):
     t = np.array([tn, tn + theta * dt])
 
     for i in range(Nx + 1):
-        v_values[i] = odeint(fitzhugh_nagumo, np.array([u0[i], w0[i]]), t)[-1][0]
-        w_values[i] = odeint(fitzhugh_nagumo, np.array([u0[i], w0[i]]), t)[-1][-1]
+        v_values[i] = odeint(derivative, [u0[i], w0[i]], t)[-1][0]
+        w_values[i] = odeint(derivative, [u0[i], w0[i]], t)[-1][-1]
 
-    u_n = Function(V)
-    u_n.vector()[:] = v_values
-    w = w_values
+    return v_values, w_values
 
 
-    # Step two
-    M_i = 1
-    lmda = 0.004  # M_e = lmda * M_i
-    gamma = float(dt * lmda / (1 + lmda))
-    if theta == 1:
-        F = (
-            u * v * dx
-            + theta * (gamma * dot(M_i * grad(u), grad(v)) * dx)
-            - u_n * v * dx
-        )
-    else:
-        F = (
-            u * v * dx
-            + theta * (gamma * dot(M_i * grad(u), grad(v)) * dx)
-            - u_n * v * dx
-            + (1 - theta) * (gamma * dot(M_i * grad(u_n), grad(v))) * dx
-        )
-
-    a, L = lhs(F), rhs(F)
-
-    u = Function(V)  # u from step 2, inital value for step 3
-    solve(a == L, u)
-
-
-    # Step three
-    if theta == 0.5:
-        u_n = u.vector()[:]
-        t = np.array([tn + theta * dt, tn + dt])
-
-        for i in range(Nx + 1):
-            v_values[i] = odeint(fitzhugh_nagumo, np.array([u_n[i], w0[i]]), t)[-1][0]
-            w_values[i] = odeint(fitzhugh_nagumo, np.array([u_n[i], w0[i]]), t)[-1][-1]
-
-        u_new = Function(V)
-        u_new.vector()[:] = v_values
-        u = u_new
-        w = w_values
-
-
-
-    return u, w
-
-
-def run_step(make_gif):
+def run_solver(make_gif):
 
     theta = 1  # =0.5 Strang/CN and N must be large, =1 Godunov/BE
-    N = 100
-    Nx = 100
+    N = 200
+    Nx = 200
     Ny = None
-    T = 400  # [s]
+    T = 400.0  # [s]
     dt = T / N  # [s]
     degree = 1
+    t = np.linspace(0, T, N+1)
 
     tn = 0
     count = 0
-    skip_frames = 99
-
-    t = np.linspace(0,T,N+1)
-
+    skip_frames = 5
 
     mesh = UnitIntervalMesh(Nx)
     V = FunctionSpace(mesh, "P", degree)
 
-    v_values = []
-    w_values = []
-
     u0, w0 = set_initial_condition(Nx, V, mesh, tn, theta, dt)
-    v_values.append(u0)
-    w_values.append(w0)
+
+    derivative = fitzhugh_nagumo
 
     for i in range(N + 1):
-        print("tn: %0.4f / %0.4f" % (tn, T))
-        #print(u0)
-
-        u, w = step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta)
+        print("tn: %0.4f / %0.4f   %s" % (tn, T, "fitzhugh_nagumo"))
+        u, w = step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta, derivative)
         tn += dt
-
-        u0 = u.vector()[:]
+        u0 = u
         w0 = w
-
-        v_values.append(u.vector()[:])
-        w_values.append(w)
 
 
         if i == count:
             # Create and save every skip_frames'th plots to file
             plt.clf()
-            plt.plot(t, u.vector()[:], label="v")
-            plt.plot(t, w, label="w")
-            plt.axis([0, 400, -1, 1])
+            plt.plot(t, u)
+            plt.axis([0, 400, -0.5, 1.5])
             plt.title("i=%d" % i)
-            plt.legend()
             plt.savefig(f"plots/u{i:04d}.png")
             count += skip_frames
 
-    np.save("v", v_values)
-    np.save("w", w_values)
 
     if make_gif:
 
@@ -173,7 +117,7 @@ def run_step(make_gif):
         # Create GIF
         img.save(fp=filepath_out,
             format="GIF",
-            append_images=imgs[1:],
+            append_images=imgs,
             save_all=True,
             duration=200,
             loop=0,
@@ -184,5 +128,4 @@ def run_step(make_gif):
 
 
 if __name__ == "__main__":
-    #run_step(make_gif=True)
-    run_step(make_gif=False)
+    run_solver(make_gif=True)
