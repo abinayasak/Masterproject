e4d51e797b45bf504e1069440378703d19aa8064
Abinaya Sakthivel <40523408+abinayasak@users.noreply.github.com>
2021-03-15 12:19:45 +0100

    Add files via upload

--- bidomain_solver.py
+++ bidomain_solver.py
@@ -17,44 +17,14 @@ def fitzhugh_nagumo_reparameterized(v, t):
     dVdt = c1*(v[0] - v_rest)*(v[0] - v_th)*(v_peak - v[0])/(v_amp**2) - (c2*(v[0] - v_rest)*v[1])/v_amp
     dWdt = b*(v[0] - v_rest - c3*v[1])
 
+
     if t >= 50 and t <= 60:
         dVdt += I_app
 
     return dVdt, dWdt
 
 
-def set_initial_condition(V, mesh):
-    coordinates = []
-    element = V.element()
-    for cell in cells(mesh):
-        for i in range(int(element.tabulate_dof_coordinates(cell).size / 2)):
-            coordinates.append((element.tabulate_dof_coordinates(cell)[i]).tolist())
-
-    coord = []
-    x0 = []
-    y0 = []
-    for i in coordinates:
-        if i not in coord:
-            coord.append(i)
-            x0.append(i[0])
-            y0.append(i[1])
-
-    #print(x0)
-    #print(y0)
-
-    x0 = np.array(sorted(x0))  # Sorting the x coordinates
-    y0 = np.array(sorted(y0))  # Sorting the y coordinates
-    np.save("x0", x0); np.save("y0", y0)
-    u0 = x0
-    u0[u0 < 0.2] = -0.0  # if x < 0.2, set u0 = 0.
-    u0[u0 >= 0.2] = -85.0  # if x >= 0.2, set u0 = -85.
-
-    w0 = np.zeros(len(u0))
-
-    return u0, w0
-
-
-def bidomain_model(V, theta, u_1, u_2, v_1, v_2, u_n1, u_n2, dt):
+def bidomain_model(V, u_n, dt):
     sigma_e = 0.62                             # [Sm^-1]
     sigma_i = 0.17                             # [Sm^-1]
     sigma = sigma_i*sigma_e/(sigma_e+sigma_i)  # [Sm^-1]
@@ -65,12 +35,19 @@ def bidomain_model(V, theta, u_1, u_2, v_1, v_2, u_n1, u_n2, dt):
     lmda = 0.004
     M_e = lmda * M_i
 
+    u_1 = TrialFunction(V)
+    v_1 = TestFunction(V)
+
+    u_2 = TrialFunction(V)
+    v_2 = TestFunction(V)
+
+    u_n1 = u_n
 
     F = (
         u_1 * v_1 * dx
         + dt * (dot(M_i * grad(u_2), grad(v_2)) * dx)
-        - dt * (dot(M_e * grad(u_2), grad(v_2)) * dx)
-        - u_n1 * v_1 * dx
+        + dt * (dot(M_e * grad(u_2), grad(v_2)) * dx)
+        - (u_n1 * v_1 * dx)
     )
 
     a, L = lhs(F), rhs(F)
@@ -81,37 +58,24 @@ def bidomain_model(V, theta, u_1, u_2, v_1, v_2, u_n1, u_n2, dt):
     return u
 
 
-
 def step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta, derivative):
+    u0 = np.array(u0)
+    w0 = np.array(w0)
 
-    v_1, v_2 = TestFunctions(V)
-
-    u = Function(V)
-    u_n = Function(V)
-
-    u_1, u_2 = split(u)
-    u_n1, u_n2 = split(u_n)
-
-
-    #u_1, u_2 = u.split(deepcopy=True)
     # Step one
-    v_values = np.zeros(u.vector()[:].size)
-    w_values = np.zeros(u.vector()[:].size)
+    v_values = np.zeros(len(u0))
+    w_values = np.zeros(len(u0))
 
     t = np.array([tn, tn + theta * dt])
-    for i in range(Nx + 1):
+    for i in range(len(u0)):
         v_values[i], w_values[i] = odeint(derivative, [u0[i], w0[i]], t)[-1]
 
-    print(v_values.size)
-    print(v_values)
     u_n = Function(V)
-    print(u_n.vector()[:].size)
-    u_n1, u_n2 = u_n.split(deepcopy=True)
-    print(u_n1.vector()[:].size)
+    u_n.vector()[:] = v_values
 
 
     # Step two
-    u = bidomain_model(V, theta, u_1, u_2, v_1, v_2, u_n1, u_n2, dt)
+    u = bidomain_model(V, u_n, dt)
 
 
     # Step three
@@ -130,62 +94,68 @@ def step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta, derivative):
 
 
     return u, w_values
-    #return v_values, w_values
 
 
-def run_solver(make_gif):
+
+def run_solver(make_gif, dimension):
 
     theta = 1  # =0.5 Strang/CN and N must be large, =1 Godunov/BE
     degree = 1
-    N = 100
-    Nx = 10
-    Ny = 10
-    T = 100.0                       # [ms]
+    N = 200
+    Nx = 20
+    Ny = 20
+    T = 500.0                       # [ms]
     dt = T / N                      # [ms]
     t = np.linspace(0, T, N+1)      # [ms]
-    #mesh = RectangleMesh(Point(0, 0), Point(20, 7), Nx, Ny)
-    mesh = UnitSquareMesh(Nx, Ny)
-    P1 = FiniteElement('P', triangle, degree)
-    element = MixedElement([P1, P1])
-    V = FunctionSpace(mesh, element)
 
-    u0 = Expression('x[0] <= 2.0 ? 0 : -85', degree=0)
-    u_n = interpolate(u0, V)
+    if dimension == "1D":
+        mesh = IntervalMesh(Nx, 0, 20)
+        u0 = Expression('x[0] <= 2.0 ? 0 : -85', degree=0) #(condition) ? (if_true) : if_false)
+    if dimension == "2D":
+        mesh = RectangleMesh(Point(0, 0), Point(20, 20), Nx, Ny)
+        u0 = Expression('(x[0] <= 2.0 && x[1] <= 2.0) ? 0 : -85', degree=0)
 
+    V = FunctionSpace(mesh, "P", degree)
+    u0 = interpolate(u0, V)
+    #print(u0.vector()[:])
 
-    #print(u_n.vector()[:])
-    #u0 = u_n.vector()[:]
+    x0 = Expression('x[0]', degree=0)
+    x0 = interpolate(x0, V)
+    np.save("x0", x0.vector()[:])
 
-    #print(u0)
-    #u0 = u0[::-1]
-    #w0 = np.zeros(len(u0))
+    u0 = u0.vector()[:]
+    w0 = np.zeros(len(u0))
 
-    #u0, w0 = set_initial_condition(V, mesh)
-    #derivative = fitzhugh_nagumo_reparameterized
+    derivative = fitzhugh_nagumo_reparameterized
 
-
-
-    """
     tn = 0
     count = 0
-    skip_frames = 10
+    skip_frames = 5
     for i in range(N + 1):
         print("tn: %0.4f / %0.4f" % (tn, T))
         u, w = step(V, T, N, dt, tn, Nx, Ny, degree, u0, w0, theta, derivative)
         tn += dt
         u0 = u.vector()[:]
         w0 = w
-        break
 
         if make_gif:
             if i == count:
                 # Create and save every skip_frames'th plots to file
                 plt.clf()
-                plt.plot(t, u.vector()[:], label="v")
-                #plt.plot(t, u, label="v")
-                plt.plot(t, w, label="w")
-                plt.axis([0, 400, -100, 100])
-                plt.legend()
+                if dimension == "1D":
+                    plt.plot(x0.vector()[:], u.vector()[:][::-1], label="v")
+                    plt.plot(x0.vector()[:], w[::-1], label="w")
+                    plt.axis([0, 20, -100, 100])
+                    plt.legend()
+                    plt.xlabel("[mm]")
+                    plt.ylabel("[mV]")
+
+                if dimension == "2D":
+                    c = plot(u, mode='color', vmin=-85, vmax=40)
+                    plt.colorbar(c, orientation='vertical')
+                    plt.xlabel("x [mm]")
+                    plt.ylabel("y [mm]")
+
                 plt.title("i=%d" % i)
                 plt.savefig(f"plots/u{i:04d}.png")
 
@@ -194,14 +164,13 @@ def run_solver(make_gif):
 
     if make_gif:
 
-        import glob
+        import glob; import os
         from PIL import Image
-        import os
 
         filepath_in = "plots/u*.png"
-        filepath_out = "bidomain_animation.gif"
+        filepath_out = "bidomain_%s.gif" % dimension
 
-        # Collecting the plots and putting them in a list
+        # Collecting the plots and putting them in a ordered list
         img, *imgs = [(Image.open(f)) for f in sorted(glob.glob(filepath_in))]
 
         # Create GIF
@@ -213,9 +182,9 @@ def run_solver(make_gif):
             loop=0,
         )
 
-        # Delete all plots in faile after creating the GIF
+        # Delete all plots in file after creating the GIF
         [os.remove(f) for f in sorted(glob.glob(filepath_in))]
-    """
+
 
 if __name__ == "__main__":
-    run_solver(make_gif=False)
+    run_solver(make_gif=True, dimension="2D")
