26a2df043d25446a2a547629f58a34b9cc6a56d2
Abinaya Sakthivel <40523408+abinayasak@users.noreply.github.com>
2021-11-15 11:41:34 +0100

    Add files via upload

--- cbcbeat_torsomesh_2.0.py
+++ cbcbeat_torsomesh_2.0.py
@@ -19,7 +19,6 @@
 # Import the cbcbeat module
 import matplotlib.pyplot as plt
 from cbcbeat import *
-from mshr import *
 import numpy as np
 
 # Turn on FFC/FEniCS optimizations
@@ -38,13 +37,46 @@ if cbcbeat.dolfin_adjoint:
 mesh = Mesh('mesh/pre_torso.xml')
 marker = MeshFunction("size_t", mesh, mesh.topology().dim(), mesh.domains())
 
-refined_mesh = adapt(mesh)
-refined_marker = adapt(marker, refined_mesh)
+refined_mesh = mesh
+refined_marker = marker
+
+#refined_mesh = adapt(mesh)
+#refined_marker = adapt(marker, refined_mesh)
 
 heart_mesh = MeshView.create(refined_marker, 2)
 torso_mesh = MeshView.create(refined_marker, 1)
 
-vtkfile = File('mesh/submesh_torsomesh.xml')
+#refined_mesh = adapt(refined_mesh)
+#refined_marker = adapt(refined_marker, refined_mesh)
+#heart_mesh = MeshView.create(refined_marker, 2)
+
+
+"""
+plt.figure()
+plot(refined_mesh)
+plt.savefig('pictures/refined_mesh.png')
+plt.figure()
+plot(heart_mesh)
+plt.savefig('pictures/heart_mesh.png')
+
+
+plot(refined_mesh)
+plt.plot(12,18.3,'ro')
+plt.plot(12,0.3, 'bo')
+plt.plot(1.6,13.5, 'ro')
+plt.plot(25.8,13.5, 'bo')
+plt.savefig('pictures/surface_potential_points.png')
+"""
+
+"""
+mesh_cells = [refined_mesh.num_vertices(),  refined_mesh.num_cells()]
+heart_mesh_cells = [heart_mesh.num_vertices(), heart_mesh.num_cells()]
+print(mesh_cells)
+print(heart_mesh_cells)
+print("Compute min/max cell inradius.", refined_mesh.rmin(), refined_mesh.rmax())
+print("Compute min/max cell inradius.", heart_mesh.rmin(), heart_mesh.rmax())
+"""
+vtkfile = File('mesh/heart_mesh_torsomesh.xml')
 vtkfile << heart_mesh
 
 
@@ -53,7 +85,10 @@ def setup_conductivities(mesh, chi, C_m):
     # Load fibers and sheets
     Vv = VectorFunctionSpace(mesh, "DG", 0)
     fiber = Function(Vv)
-    File("fibers/fiber.xml") >> fiber
+    File("fibers/fiber_torso.xml") >> fiber
+    plt.figure()
+    plot(fiber)
+    plt.savefig('pictures/fiber_direction')
 
     # Extract stored conductivity data.
     V = FunctionSpace(mesh, "CG", 1)
@@ -64,10 +99,10 @@ def setup_conductivities(mesh, chi, C_m):
     g_il_field = Function(V, name="g_il")
     g_it_field = Function(V, name="g_it")
 
-    g_el_field.vector()[:] = 2.0/(C_m*chi)
-    g_et_field.vector()[:] = 1.65/(C_m*chi)
-    g_il_field.vector()[:] = 3.0/(C_m*chi)
-    g_it_field.vector()[:] = 1.0/(C_m*chi)
+    g_el_field.vector()[:] = 2.0/(C_m*chi) #2.0/(C_m*chi)
+    g_et_field.vector()[:] = 1.65/(C_m*chi) #1.65/(C_m*chi)
+    g_il_field.vector()[:] = 3.0/(C_m*chi) #3.0/(C_m*chi)
+    g_it_field.vector()[:] = 1.0/(C_m*chi) #1.0/(C_m*chi)
 
     # Construct conductivity tensors from directions and conductivity
     # values relative to that coordinate system
@@ -81,15 +116,15 @@ def setup_conductivities(mesh, chi, C_m):
 
     return M_i, M_e
 
-chi = 400
+chi = 90
 C_m = 1.0
 
 M_i, M_e = setup_conductivities(heart_mesh, chi, C_m)
 M_T = 1.0/(C_m*chi)
 
 
-#cell_model = Tentusscher_panfilov_2006_epi_cell()
-cell_model = FitzHughNagumoManual()
+cell_model = Tentusscher_panfilov_2006_epi_cell()
+#cell_model = FitzHughNagumoManual()
 
 # Define stimulus on three different areas on the torso mesh
 time = Constant(0.0)
@@ -128,15 +163,13 @@ I_s3 = Expression("time >= start ? (time <= (duration + start) ? amplitude : 0.0
 # Store input parameters in cardiac model
 stimulus = Markerwise((I_s1,I_s2,I_s3), (1,2,3), S1_markers)
 
-#print(stimulus.markers())
-
 # Collect this information into the CardiacModel class
 cardiac_model = CardiacModel(refined_mesh, heart_mesh, time, M_i, M_e, M_T, cell_model, stimulus)
 
 # Customize and create a splitting solver
 ps = SplittingSolver.default_parameters()
 ps['apply_stimulus_current_to_pde'] = True
-ps["theta"] = 1.0
+ps["theta"] = 0.5
 ps["pde_solver"] = "bidomain"
 ps["CardiacODESolver"]["scheme"] = "RL1" # 1st order Rush-Larsen for the ODEs
 
@@ -148,64 +181,66 @@ vs_.assign(cell_model.initial_conditions())
 
 # Time stepping parameters
 N = 200
-T = 900.
-#dt = 0.25
+T = 400
 dt = T/N
-#N = int(T/dt)
 interval = (0.0, T)
-
 
 out_v = File("paraview_cbcbeat/bidomain_v.pvd")
 out_u = File("paraview_cbcbeat/bidomain_u.pvd")
 
+
 count = 0
 u_difference = np.zeros((2,N))
 t = np.zeros(N)
+action_potential = np.zeros(N)
 plot_figures = True
-#plot_meshes = [dt*500, dt*1500, dt*2000, dt*3500]
-plot_meshes = [dt*(N/4.), dt*(N/3.), dt*(N/2.), dt*(N/1.)]
+plotting_time = [25.0, 75.0, 220.0, 290.0, 360.0]
 for (timestep, fields) in solver.solve(interval, dt):
     print("(t_0, t_1) = (%g, %g)", timestep)
     # Extract the components of the field (vs_ at previous timestep,
     # current vs, current vur)
     (vs_, vs, vur) = fields
 
+
+    action_potential[count] = vur.sub(0)(12,14)
     t[count] = timestep[1]
-    u_difference[0][count] = vur.sub(1)(10,19.1) - vur.sub(1)(10,0.4)
-    u_difference[1][count] = vur.sub(1)(2,10) - vur.sub(1)(25.5,10)
-    #print(u_difference[0][count], u_difference[1][count])
+    u_difference[0][count] = vur.sub(1)(10,19) - vur.sub(1)(10,0.3)
+    u_difference[1][count] = vur.sub(1)(1.6,10) - vur.sub(1)(25.8,10)
+
     count += 1
     out_v << vur.sub(0)
     out_u << vur.sub(1)
 
     if plot_figures == True:
-        for time in plot_meshes:
+        for time in plotting_time:
             if timestep[0] == time:
+                print('PLOTTING FIGURE')
                 plt.figure()
                 c = plot(vur.sub(0), title="v at time=%d ms" %(time), mode='color', vmin=-100, vmax=50)
                 c.set_cmap("jet")
                 plt.colorbar(c, fraction=0.043, pad=0.009)
                 plt.savefig("plots_cbcbeat/torsomesh_v_%d.png" %(time))
                 plt.figure()
-                c = plot(vur.sub(1), title="u_e at time=%d ms" %(time), mode='color', vmin=-100, vmax=50)
+                c = plot(vur.sub(1), title="u_e at time=%d ms" %(time), mode='color', vmin=-10, vmax=10)
                 c.set_cmap("jet")
                 plt.colorbar(c, fraction=0.034, pad=0.009)
                 plt.savefig("plots_cbcbeat/torsomesh_u_e_%d.png" %(time))
 
 
-
-
-
+np.save("u_difference.npy", u_difference)
+np.save("action_potential", action_potential)
+np.save("t", t)
 
 def plot_ECG():
     plt.figure()
-    plt.plot(t, u_difference[0], "m", label="Normal: top-to-bottom")
-    plt.plot(t, u_difference[1], "k", label="Normal: left-to-right")
-    plt.axis([0,900,-15,15])
+    plt.plot(t, u_difference[0], "m", label="top-to-bottom")
+    plt.plot(t, u_difference[1], "k", label="left-to-right")
     plt.xlabel("ms")
     plt.ylabel("mV")
-    plt.title("Potential difference")
+    plt.title("Surface potential difference")
     plt.legend()
-    plt.savefig("plots_cbcbeat/ecg_plot.png")
+    plt.savefig("plots_cbcbeat/surface_potential.png")
+
+
 
 plot_ECG()
--- cbcbeat_unitsquaremesh_2.0.py
+++ cbcbeat_unitsquaremesh_2.0.py
@@ -19,6 +19,7 @@
 # Import the cbcbeat module
 import matplotlib.pyplot as plt
 from cbcbeat import *
+import numpy as np
 
 # Turn on FFC/FEniCS optimizations
 parameters["form_compiler"]["representation"] = "uflacs"
@@ -33,8 +34,7 @@ if cbcbeat.dolfin_adjoint:
     parameters["adjoint"]["stop_annotating"] = True
 
 # Define the computational domain
-mesh = UnitSquareMesh(5, 5)
-
+mesh = UnitSquareMesh(200, 200)
 time = Constant(0.0)
 
 def circle_heart(x,y):
@@ -50,14 +50,43 @@ for c in cells(mesh):
 
 heart_mesh = MeshView.create(marker, 1)
 
-vtkfile = File("heart_mesh_unitsquaremesh.xml")
+
+
+mesh_cells = [mesh.num_vertices(),  mesh.num_cells()]
+heart_mesh_cells = [heart_mesh.num_vertices(), heart_mesh.num_cells()]
+"""
+print(mesh_cells)
+print(heart_mesh_cells)
+print("Compute min/max cell inradius.", mesh.rmin(), mesh.rmax())
+print("Compute min/max cell inradius.", heart_mesh.rmin(), heart_mesh.rmax())
+"""
+#[40401, 80000]
+#[8049, 15718]
+#Compute min/max cell inradius. 0.001464466094037834 0.0014644660940898947
+#Compute min/max cell inradius. 0.0014644660940508409 0.0014644660940898622
+
+
+"""
+plt.figure()
+plot(mesh)
+plt.savefig('pictures/unitsquaremesh.png')
+plt.figure()
+plot(heart_mesh)
+plt.savefig('pictures/circle_submesh.png')
+"""
+
+
+vtkfile = File("mesh/heart_mesh_unitsquaremesh.xml")
 vtkfile << heart_mesh
 
 def setup_conductivities(mesh, chi, C_m):
     # Load fibers and sheets
     Vv = VectorFunctionSpace(mesh, "DG", 0)
     fiber = Function(Vv)
-    File("fiber_unitsquaremesh.xml") >> fiber
+    File("fibers/fiber_unitsquare.xml") >> fiber
+    plt.figure()
+    plot(fiber)
+    plt.savefig('pictures/fiber_direction_unitsquaremesh')
 
     # Extract stored conductivity data.
     V = FunctionSpace(mesh, "CG", 1)
@@ -85,37 +114,35 @@ def setup_conductivities(mesh, chi, C_m):
 
     return M_i, M_e
 
-
-"""# Define the conductivity (tensors)
-sigma_e = 0.62                      # [Sm^-1]
-sigma_i = 0.17                      # [Sm^-1]
-chi = 2000                          # [cm^-1]       torso mesh
-C_m = 1.0                           # [mu*F*cm−2]   torso mesh
-M_i = (sigma_i)/(C_m*chi)
-M_e = (sigma_e)/(C_m*chi)
-M_T = 0.25*M_e"""
-
-chi = 400
+chi = 90
 C_m = 1.0
-
 M_i, M_e = setup_conductivities(heart_mesh, chi, C_m)
 M_T = 1.0/(C_m*chi)
 
+
+
+"""
+# Define the conductivity (tensors)
+sigma_e = 0.62                      # [Sm^-1]
+sigma_i = 0.17                      # [Sm^-1]
+chi = 1400.                         # [cm^-1]
+C_m = 1.0                           # [mu*F*cm^−2]
+M_i = (sigma_i)/(C_m*chi)
+M_e = (sigma_e)/(C_m*chi)
+M_T = 1./(C_m*chi)
+"""
 
 # Pick a cell model (see supported_cell_models for tested ones)
 cell_model = FitzHughNagumoManual()
 
 
-S1_subdomain = CompiledSubDomain("(pow(x[0] - 0.5, 2) + pow(x[1] - 0.55, 2)) <= pow(0.1, 2)", degree=2)
+S1_subdomain = CompiledSubDomain("(pow(x[0] - 0.5, 2) + pow(x[1] - 0.55, 2)) <= pow(0.15, 2)", degree=2)
 S1_markers = MeshFunction("size_t", heart_mesh, heart_mesh.topology().dim())
 S1_subdomain.mark(S1_markers, 1)
 
-
-# Define stimulation (NB: region of interest carried by the mesh
-# and assumptions in cbcbeat)
-duration = 5. # ms
-
-amplitude = 10 #60 # mV/ms
+# Define stimulation
+duration = 5.  # ms
+amplitude = 10 # mV/ms
 I_s = Expression("time >= start ? (time <= (duration + start) ? amplitude : 0.0) : 0.0",
                   time=time,
                   start=0.0,
@@ -127,17 +154,15 @@ stimulus = Markerwise((I_s,), (1,), S1_markers)
 
 
 # Collect this information into the CardiacModel class
-cardiac_model = CardiacModel(mesh, heart_mesh, mesh, time, M_i, M_e, M_T, cell_model, stimulus)
+cardiac_model = CardiacModel(mesh, heart_mesh, time, M_i, M_e, M_T, cell_model, stimulus)
+
 
 # Customize and create a splitting solver
 ps = SplittingSolver.default_parameters()
 ps['apply_stimulus_current_to_pde'] = True
-ps["theta"] = 1.0                        # Second order splitting scheme
-ps["pde_solver"] = "bidomain"          # Use Monodomain model for the PDEs
-ps["CardiacODESolver"]["scheme"] = "RL1" # 1st order Rush-Larsen for the ODEs
-ps["BidomainSolver"]["linear_solver_type"] = "iterative"
-ps["BidomainSolver"]["algorithm"] = "cg"
-ps["BidomainSolver"]["preconditioner"] = "petsc_amg"
+ps["theta"] = 0.5
+ps["pde_solver"] = "bidomain"
+ps["CardiacODESolver"]["scheme"] = "RL1"
 
 solver = SplittingSolver(cardiac_model, params=ps)
 
@@ -146,36 +171,36 @@ solver = SplittingSolver(cardiac_model, params=ps)
 vs_.assign(cell_model.initial_conditions())
 
 # Time stepping parameters
-N = 200
-T = 500.
+N = 3200
+T = 400.
 dt = T/N
 interval = (0.0, T)
-
 
 out_v = File("paraview_cbcbeat/bidomain_v.pvd")
 out_u = File("paraview_cbcbeat/bidomain_u.pvd")
 
-time = 320
+plot_figures = True
+plotting_time = [25.0, 75.0, 220.0, 290.0, 360.0]
 for (timestep, fields) in solver.solve(interval, dt):
     print("(t_0, t_1) = (%g, %g)", timestep)
     # Extract the components of the field (vs_ at previous timestep,
     # current vs, current vur)
     (vs_, vs, vur) = fields
 
+    #out_v << vur.sub(0)
+    #out_u << vur.sub(1)
 
-    out_v << vur.sub(0)
-    out_u << vur.sub(1)
-
-    if timestep[0] == time:
-        plt.figure()
-        #plot(vur.sub(0), title="v at T = 600 ms")
-        c = plot(vur.sub(0), title="v at time=%d ms" %(time), mode='color', vmin=-100, vmax=50)
-        c.set_cmap("jet")
-        plt.colorbar(c, orientation='vertical')
-        plt.savefig("v_%d.png" %(time))
-        plt.figure()
-        #plot(vur.sub(1), title="u_e at T = 600 ms")
-        c = plot(vur.sub(1), title="u_e at time=%d ms" %(time), mode='color', vmin=-100, vmax=50)
-        c.set_cmap("jet")
-        plt.colorbar(c, orientation='vertical')
-        plt.savefig("u_e_%d.png" %(time))
+    if plot_figures == True:
+        for time in plotting_time:
+            if timestep[0] == time:
+                print('PLOTTING FIGURE')
+                plt.figure()
+                c = plot(vur.sub(0), title="v at time=%d ms" %(time), mode='color', vmin=-100, vmax=50)
+                c.set_cmap("jet")
+                plt.colorbar(c, orientation='vertical')
+                plt.savefig("plots_cbcbeat/unitsquaremesh_v_%d.png" %(time))
+                plt.figure()
+                c = plot(vur.sub(1), title="u_e at time=%d ms" %(time), mode='color', vmin=-10, vmax=10)
+                c.set_cmap("jet")
+                plt.colorbar(c, orientation='vertical')
+                plt.savefig("plots_cbcbeat/unitsquaremesh_u_e_%d.png" %(time))
